d6419d97c14d8be23fa89184b34a5f93545b2fce
Aburasumashi81104 <boostedzombie@gmail.com>
2018-02-22 12:46:40 +0900

    2-1

--- Practice/src/practice01/PTra01_07.java
+++ Practice/src/practice01/PTra01_07.java
@@ -21,24 +21,24 @@ public class PTra01_07 {
 
 		// ★ それぞれの型変数に、値を代入してください
 		// ※ 値は、好きな数字を入力してください
-
-
-
-
-
-
-
-
+		bo = true;
+		by = 8;
+		s = 10;
+		c = 'S';
+		i = 2018;
+		l = 1234567890987654321L;
+		f = 9.11F;
+		d = 3.1415926535;
 
 		// ★ それぞれの変数の中身を出力してください
 
-
-
-
-
-
-
-
-
+		System.out.println(bo);
+		System.out.println(by);
+		System.out.println(s);
+		System.out.println(c);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
 	}
 }
