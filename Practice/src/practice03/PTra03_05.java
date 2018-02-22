76d0203642de4884d292012fa1cd2e7028fa491c
Aburasumashi81104 <boostedzombie@gmail.com>
2018-02-22 15:11:40 +0900

    3-9

--- Practice/src/practice03/PTra03_05.java
+++ Practice/src/practice03/PTra03_05.java
@@ -34,5 +34,15 @@ public class PTra03_05 {
 		 * ※ プログラムは何行書いても良いです
 		 */
 
+		if(num >= 75) {
+			System.out.println("雨が降る確率が高いです");
+		}
+		else if(num<30){
+			System.out.println("雨はまず降らないでしょう");
+		}
+		else {
+			System.out.println("雨が降るかもしれないです");
+		}
+
 	}
 }
