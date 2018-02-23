93d9d77577032293a92d1c4a5df3c5e6aac9593b
Aburasumashi81104 <boostedzombie@gmail.com>
2018-02-23 12:29:32 +0900

    6-7

--- Practice/src/practice06/PTra06_06.java
+++ Practice/src/practice06/PTra06_06.java
@@ -20,13 +20,19 @@ public class PTra06_06 {
 		 * ■■■■□
 		 */
 		for(int i = 0; i < 5; i++) {
-			for(int j = i; j < 5; j++) {
-				System.out.print("□");
-			for(int k = j; k < 4; k++) {
-				System.out.println();
+
+			for(int k = 4-i; k < 4; k++) {
 				System.out.print("■");
 			}
+
+			for(int j = i; j < 5; j++) {
+				System.out.print("□");
 			}
+
+
+			System.out.println();
+
+
 		}
 	}
 }
--- Practice/src/practice06/PTra06_07.java
+++ Practice/src/practice06/PTra06_07.java
@@ -25,5 +25,34 @@ public class PTra06_07 {
 		 * ■□□□□
 		 */
 
+
+		for(int i = 0; i < 5; i++) {
+
+			for(int k = 4-i; k < 4; k++) {
+				System.out.print("■");
+			}
+
+			for(int j = i; j < 5; j++) {
+				System.out.print("□");
+			}
+
+			System.out.println();
+		}
+
+		for(int i = 0; i < 5; i++) {
+
+
+
+			for(int j = i; j < 5; j++) {
+				System.out.print("■");
+			}
+
+			for(int k = 4-i; k < 4; k++) {
+				System.out.print("□");
+			}
+
+			System.out.println();
+
+		}
 	}
 }
