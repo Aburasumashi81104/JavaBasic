a7ee27c44bdaa621888c76f550cff54dee0fc95d
Aburasumashi81104 <boostedzombie@gmail.com>
2018-02-26 11:18:33 +0900

    10-1

--- Practice/src/practice10/PTra10_01.java
+++ Practice/src/practice10/PTra10_01.java
@@ -26,11 +26,15 @@ public class PTra10_01 {
 
 		// Userクラスを作成後に着手してください
 		// ★ User型の変数usを宣言してください
-
+		User us;
 		// ★ 変数usに、Userクラスのインスタンスを作成・代入してください
-
+		us = new User();
 
 		// ★ 変数usに格納されているインスタンスの、フィールドuserId, userNm, mail, passwordを出力してください
+		System.out.println("ユーザーIDは"+us.userId);
+		System.out.println("ユーザー名は"+us.userNm);
+		System.out.println("メールアドレスは"+us.mail);
+		System.out.println("パスワードは"+us.password);
 
 	}
 }
---
+++ Practice/src/practice10/User.java
@@ -0,0 +1,16 @@
+package practice10;
+/*
+ * PTra10_01.java
+ *   作成	LIKEIT	2017
+ *------------------------------------------------------------
+ * Copyright(c) Rhizome Inc. All Rights Reserved.
+ */
+
+public class User {
+
+	int userId = 001;
+	String userNm = "やる夫";
+	String mail = "2ch@2ch.com";
+	String password = "news4vip";
+
+}
