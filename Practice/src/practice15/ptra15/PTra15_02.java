49d06134b1ea125378d01802acaac6c71396dfa6
Aburasumashi81104 <boostedzombie@gmail.com>
2018-03-01 15:34:14 +0900

    15-2

--- Practice/src/practice15/ptra15/PTra15_02.java
+++ Practice/src/practice15/ptra15/PTra15_02.java
@@ -13,11 +13,15 @@ public class PTra15_02 {
 	public static void main(String[] args) {
 
 		Room room = new Java2Month();
+		Java2Month java2month = (Java2Month)room;
+		/*Room型にJava2Monthのインスタンスを入れている*/
+		/*Java2Month型にキャスト*/
 
 		// roomに格納されたJava2MonthインスタンスのgetCurriculumメソッドを呼び出してください
-
-
+		String[] array = java2month.getCurriculum();
 		// 取得したカリキュラム情報を全て出力してください
-
+		for (String roomarray : array) {
+			System.out.println(roomarray);
+		}
 	}
 }
