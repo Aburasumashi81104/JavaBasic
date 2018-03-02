f256e02c8809435dcbb62907e85de460cff09d05
Aburasumashi81104 <boostedzombie@gmail.com>
2018-03-02 18:38:16 +0900

    18-2

--- Practice/src/practice18/PTra18_02.java
+++ Practice/src/practice18/PTra18_02.java
@@ -1,10 +1,17 @@
 /*
- * PTra18_02.java
+ * PTra18_02.java.add
  *   作成	LIKEIT	2017
  *------------------------------------------------------------
  * Copyright(c) Rhizome Inc. All Rights Reserved.
  */
 package practice18;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import entity.Player;
 
 public class PTra18_02 {
 
@@ -32,9 +39,44 @@ public class PTra18_02 {
 		 * ★ ArrayListを作成して、Playerインスタンスを格納してください
 		 */
 
+		String[] fileRow = new String[65536];
+
+		int index = 0;
+		ArrayList<Player> playerList = new ArrayList<>();
+		/*playerインスタンスを格納したいのでPlayer型のArrayListを作る*/
+        try(Scanner scanner = new Scanner(new File("file/BestElevenCandidate.csv"))) {
+            while (scanner.hasNext()) {
+            	fileRow[index] = scanner.nextLine();
+            	index ++;
+            	/*一行ごとにぶち込む*/
+            }
+			for (int i = 0; i < index; i++) {
+				String[] rowArray = fileRow[i].split(",");
+				/*行単位で入っているものをカンマで区切ってString配列に入れる*/
+				Player player = new Player();
+				/*上で宣言してしまうとインスタンスは一度きりしか作成されないので*/
+				/*繰り返し文のなかでインスタンスを作成する*/
+				player.setPosition(rowArray[0]);
+				player.setName(rowArray[1]);
+				player.setCountry(rowArray[2]);
+				player.setTeam(rowArray[3]);
+
+				playerList.add(player);
+				/*上で各フィールドに値を入れたplayerインスタンス自体をArrayListに格納*/
+			}
+			/*for文を抜けてplayerListが完成*/
+
+        } catch (FileNotFoundException e) {
+            System.out.println("ファイルが見つかりません");
+        }
 
 		// ★ ArrayListに格納されているインスタンス全てのtoStringメソッドを実行し、出力してください
 		// ※ できれば拡張for文を使いましょう
+
+        for(Player p:playerList) {
+            System.out.println(p.toString());
+            /*繰り返しでArrayListの中身を表示*/
+        }
 
 	}
 }
