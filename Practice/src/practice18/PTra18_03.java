2a36efd21cc6677cd4107568ada31af574dd6dd4
Aburasumashi81104 <boostedzombie@gmail.com>
2018-03-05 10:26:53 +0900

    18-2

--- Practice/src/practice18/PTra18_03.java
+++ Practice/src/practice18/PTra18_03.java
@@ -6,6 +6,13 @@
  */
 package practice18;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import entity.Player;
+
 public class PTra18_03 {
 
 	/*
@@ -19,13 +26,43 @@ public class PTra18_03 {
 		 * ★ file/BestElevenCandidate.csvの内容を取得し、１行毎にPlayerインスタンスに情報を格納してください
 		 * ★ ArrayListを作成して、Playerインスタンスを格納してください
 		 */
+		String[] fileRow = new String[65536];
 
+		int index = 0;
+		ArrayList<Player> playerList = new ArrayList<>();
+        try(Scanner scanner = new Scanner(new File("file/BestElevenCandidate.csv"))) {
+            while (scanner.hasNext()) {
+            	fileRow[index] = scanner.nextLine();
+            	index ++;
+
+            }
+			for (int i = 0; i < index; i++) {
+				String[] rowArray = fileRow[i].split(",");
+
+				Player player = new Player();
+
+				player.setPosition(rowArray[0]);
+				player.setName(rowArray[1]);
+				player.setCountry(rowArray[2]);
+				player.setTeam(rowArray[3]);
+
+				playerList.add(player);
+
+			}
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("ファイルが見つかりません");
+        }
 
 		// ★ ①のArrayListの中から"レアル・マドリード", "バルセロナ"の選手を除外してください
-
+        playerList.remove(playerList.indexOf("レアル・マドリード"));
 
 		// ★ 削除後のArrayListの中身を全件出力してください
-
+        for(Player p:playerList) {
+            System.out.println(p.toString());
+            /*繰り返しでArrayListの中身を表示*/
+        }
 
 	}
 }
--- Practice/src/practice18/PTra18_04.java
+++ Practice/src/practice18/PTra18_04.java
@@ -6,6 +6,13 @@
  */
 package practice18;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import entity.Player;
+
 public class PTra18_04 {
 
 	/*
@@ -20,6 +27,32 @@ public class PTra18_04 {
 		 * ★ ArrayListを作成して、Playerインスタンスを格納してください
 		 */
 
+		String[] fileRow = new String[65536];
+
+		int index = 0;
+		ArrayList<Player> playerList = new ArrayList<>();
+        try(Scanner scanner = new Scanner(new File("file/BestElevenCandidate.csv"))) {
+            while (scanner.hasNext()) {
+            	fileRow[index] = scanner.nextLine();
+            	index ++;
+            }
+			for (int i = 0; i < index; i++) {
+				String[] rowArray = fileRow[i].split(",");
+
+				Player player = new Player();
+
+				player.setPosition(rowArray[0]);
+				player.setName(rowArray[1]);
+				player.setCountry(rowArray[2]);
+				player.setTeam(rowArray[3]);
+
+				playerList.add(player);
+
+			}
+
+        } catch (FileNotFoundException e) {
+            System.out.println("ファイルが見つかりません");
+        }
 
 		// ★ ①のArrayListの中からGK1名、DF4名、MF4名, FW2名をランダムで出力してください
 
